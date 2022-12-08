349adca3326e28e688fdc7008a1a75e33df1194d
Akbar <makbarali1105@gmail.com>
2022-12-08 11:40:21 -0500

    Second Commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,15 +2,20 @@
 import { useRouter } from "vue-router";
 const router = useRouter()
 
-        
+
 </script>
 
 <template>
     <div>
+        <RouterLink to="/login" custom v-slot="{ navigate }">
+    <button @click="navigate" role="link">Login</button>
+</RouterLink>
         <h1>header</h1>
     </div>
 </template>
 
 <style scoped>
-
+h1{
+    color: white;
+}
 </style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div>
-
+        <h1>hello</h1>
     </div>
 </template>
 
